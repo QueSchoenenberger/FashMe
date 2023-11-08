49b70ac61f868ed78cde42e4169625dcb6446bfb
Leonid kaup <94688645+deralteholzmichl@users.noreply.github.com>
2023-11-08 10:51:11 +0100

    add all needed activitys

---
+++ app/src/main/java/com/lepquold/GenerateOutfitActivity.java
@@ -0,0 +1,14 @@
+package com.lepquold;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class GenerateOutfitActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_generate_outfit);
+    }
+}
---
+++ app/src/main/java/com/lepquold/RewievOutfitActivity.java
@@ -0,0 +1,14 @@
+package com.lepquold;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class RewievOutfitActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_rewiev_outfit);
+    }
+}
