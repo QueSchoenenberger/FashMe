2656b73d799ed12b5eba2232c9d7a7352b95449d
Leonid kaup <94688645+deralteholzmichl@users.noreply.github.com>
2023-11-08 14:23:19 +0100

    generateOutfitRawDesign

--- app/src/main/java/com/lepquold/GenerateOutfitActivity.java
+++ app/src/main/java/com/lepquold/GenerateOutfitActivity.java
@@ -5,6 +5,8 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 public class GenerateOutfitActivity extends AppCompatActivity {
 
@@ -12,6 +14,10 @@ public class GenerateOutfitActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_generate_outfit);
+        Spinner type = findViewById(R.id.styleSpinner);
+        ArrayAdapter<CharSequence> adapterType = ArrayAdapter.createFromResource(this, R.array.style, android.R.layout.simple_spinner_item);
+        adapterType.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        type.setAdapter(adapterType);
     }
     public void toWardrobe(){
         Intent intent = new Intent(this,WardrobeActivity.class);
