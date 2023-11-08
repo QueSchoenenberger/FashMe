235fa0bb2c13518ed0af615ba8522cb56d99412b
Leonid kaup <94688645+deralteholzmichl@users.noreply.github.com>
2023-11-08 15:55:13 +0100

    fromInputToJavaValue

--- app/src/main/java/com/lepquold/CreateClothActivity.java
+++ app/src/main/java/com/lepquold/CreateClothActivity.java
@@ -6,7 +6,17 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.EditText;
 import android.widget.Spinner;
+import android.widget.Switch;
+
+import com.google.android.material.slider.Slider;
+import com.lepquold.databinding.ActivityCreateClothBinding;
+import com.lepquold.databinding.ActivityMainBinding;
+import com.lepquold.model.BodyParts;
+import com.lepquold.model.Clothing;
+import com.lepquold.model.Style;
+import com.lepquold.model.Type;
 
 public class CreateClothActivity extends AppCompatActivity {
 
@@ -15,12 +25,12 @@ public class CreateClothActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_cloth);
 
-        Spinner type = findViewById(R.id.type);
+        Spinner type = findViewById(R.id.typeCreate);
         ArrayAdapter<CharSequence> adapterType = ArrayAdapter.createFromResource(this, R.array.type, android.R.layout.simple_spinner_item);
         adapterType.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         type.setAdapter(adapterType);
 
-        Spinner style = findViewById(R.id.style);
+        Spinner style = findViewById(R.id.styleCreate);
         ArrayAdapter<CharSequence> adapterStyle = ArrayAdapter.createFromResource(this, R.array.style, android.R.layout.simple_spinner_item);
         adapterStyle.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         style.setAdapter(adapterStyle);
@@ -49,5 +59,29 @@ public class CreateClothActivity extends AppCompatActivity {
     }
 
 
+    public void addToWardrobeClick(View view) {
+        EditText editText = (EditText) findViewById(R.id.editTextDescription);
+        String discription = editText.getText().toString();
 
+        Spinner spinnerCreate = (Spinner) findViewById(R.id.styleCreate);
+        String style = spinnerCreate.getSelectedItem().toString();
+
+        Spinner spinnerType = (Spinner) findViewById(R.id.typeCreate);
+        String type = spinnerType.getSelectedItem().toString();
+
+        Switch switch1 = (Switch) findViewById(R.id.switch1);
+        boolean waterproof = switch1.isActivated();
+
+        Slider slider = (Slider) findViewById(R.id.slider);
+        Float temperture = slider.getValue();
+
+        Type tshirt = new Type(type, BodyParts.TORSO);
+        Style styleStyle = new Style(style);
+
+        Clothing c = new Clothing(discription,temperture.doubleValue(),waterproof,styleStyle,tshirt);
+        c.toString();
+
+
+
+    }
 }
