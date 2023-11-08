f4052a67999e2f72864631b11fe77ba9536dfe86
Leonid kaup <94688645+deralteholzmichl@users.noreply.github.com>
2023-11-08 10:28:47 +0100

    frontend basics

--- app/src/main/java/com/lepquold/MainActivity.java
+++ app/src/main/java/com/lepquold/MainActivity.java
@@ -1,17 +1,25 @@
 package com.lepquold;
 
+import static android.widget.Toast.makeText;
+
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
 
 import com.lepquold.databinding.ActivityMainBinding;
-
 public class MainActivity extends AppCompatActivity {
 
     private ActivityMainBinding binding;
@@ -19,19 +27,16 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
         binding = ActivityMainBinding.inflate(getLayoutInflater());
-        setContentView(binding.getRoot());
-
-        BottomNavigationView navView = findViewById(R.id.nav_view);
-        // Passing each menu ID as a set of Ids because each
-        // menu should be considered as top level destinations.
-        AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
-                R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications)
-                .build();
-        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_activity_main);
-        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
-        NavigationUI.setupWithNavController(binding.navView, navController);
+        setContentView(R.layout.activity_main);
     }
-
+    public void toWardrobe(){
+        Intent intent = new Intent(this,WardrobeActivity.class);
+        startActivity(intent);
+    }
+    public void Click1(View view){
+        Intent intent = new Intent(this, WardrobeActivity.class);
+        startActivity(intent);
+        //toWardrobe();
+    }
 }
---
+++ app/src/main/java/com/lepquold/WardrobeActivity.java
@@ -0,0 +1,35 @@
+package com.lepquold;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import com.google.android.material.snackbar.Snackbar;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.view.View;
+
+import androidx.core.view.WindowCompat;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
+
+import com.lepquold.databinding.ActivityWardrobeBinding;
+
+import java.time.Instant;
+
+public class WardrobeActivity extends AppCompatActivity {
+    private ActivityWardrobeBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = ActivityWardrobeBinding.inflate(getLayoutInflater());
+        setContentView(R.layout.activity_wardrobe);
+    }
+    private void toMain(){
+        Intent intent = new Intent(this,MainActivity.class);
+        startActivity(intent);
+    }
+}
