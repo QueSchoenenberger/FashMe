526d47035ba8d7a363000a019384ffc36815e7ae
Leonid kaup <94688645+deralteholzmichl@users.noreply.github.com>
2023-11-13 11:25:05 +0100

    addded reading of GenerateOutfitActivity form

--- app/src/main/java/com/lepquold/CreateClothActivity.java
+++ app/src/main/java/com/lepquold/CreateClothActivity.java
@@ -12,6 +12,7 @@ import android.widget.Spinner;
 import android.widget.Switch;
 
 import com.google.android.material.slider.Slider;
+import com.lepquold.Types.Types;
 import com.lepquold.databinding.ActivityCreateClothBinding;
 import com.lepquold.databinding.ActivityMainBinding;
 import com.lepquold.model.BodyParts;
@@ -63,17 +64,50 @@ public class CreateClothActivity extends AppCompatActivity {
     public void wardrobeClick(View view){
         toWardrobe();
     }
+    public void makeErrorMessage(String error){
+
+    }
 
 
     public void addToWardrobeClick(View view) {
         EditText editText = (EditText) findViewById(R.id.editTextDescription);
         String discription = editText.getText().toString();
 
+        if (discription.length() > 1){
+            makeErrorMessage("Clothing must have a discription");
+            return;
+        }
+        Style selectedStyle = Style.Casual;
         Spinner spinnerCreate = (Spinner) findViewById(R.id.styleCreate);
         String style = spinnerCreate.getSelectedItem().toString();
+        if (style.equals("Casual")){
+            selectedStyle = Style.Casual;
+        }
+        if (style.equals("Formell-Business")){
+            selectedStyle = Style.FormellBusiness;
+        }
+        if (style.equals("Smart-Casual")){
+            selectedStyle = Style.SmartCasual;
+        }
+        if (style.equals("Leger")){
+            selectedStyle = Style.Leger;
+        }
+        if (style.equals("Sportive")){
+            selectedStyle = Style.Sportive;
+        }
+        if (style.equals("Vintage")){
+            selectedStyle = Style.Vintage;
+        }
 
         Spinner spinnerType = (Spinner) findViewById(R.id.typeCreate);
         String type = spinnerType.getSelectedItem().toString();
+        Type selectedType = null;
+        try {
+            selectedType = Types.types.get(type);
+        }catch (Exception e) {
+            makeErrorMessage("Error while selecting the type");
+            return;
+        }
 
         Switch switch1 = (Switch) findViewById(R.id.switch1);
         boolean waterproof = switch1.isActivated();
@@ -82,9 +116,8 @@ public class CreateClothActivity extends AppCompatActivity {
         Float temperture = slider.getValue();
 
         Type tshirt = new Type(type, BodyParts.TORSO);
-        Style styleStyle = new Style(style);
 
-        Clothing c = new Clothing(discription,temperture.doubleValue(),waterproof,styleStyle,tshirt);
+        Clothing c = new Clothing(discription,temperture,isActivityTransitionRunning(),selectedStyle,selectedType);
 
         // Add item
         Intent intent = new Intent(this, WardrobeActivity.class);
